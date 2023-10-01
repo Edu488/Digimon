c32eeb5efa5dc88749ac41c721e22b87aa4be3f9
edu <edu488@gmail.com>
2023-10-01 11:17:09 -0500

    primer commit

---
+++ js/DIGIMON.js
@@ -0,0 +1,6 @@
+function selecionarMastocaJugador(){
+    alert('seleccionaste tu mascota')
+}
+
+let botonMascotaJugador = document.getElementById('Boton-mascota')
+botonMascotaJugador.addEventListener('click',selecionarMastocaJugador)
