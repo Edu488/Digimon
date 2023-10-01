329e328f88e35acb2fbacb43b38a6a7264e25510
edu <edu488@gmail.com>
2023-10-01 11:18:52 -0500

    segundo commit

--- js/DIGIMON.js
+++ js/DIGIMON.js
@@ -1,6 +1,36 @@
-function selecionarMastocaJugador(){
-    alert('seleccionaste tu mascota')
-}
+    
+    //funciona para iniciar el juego
 
-let botonMascotaJugador = document.getElementById('Boton-mascota')
-botonMascotaJugador.addEventListener('click',selecionarMastocaJugador)
+    function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('Boton-mascota')
+    botonMascotaJugador.addEventListener('click',selecionarMastocaJugador)  
+    
+    }
+    //llamar la respuesta de input
+    function selecionarMastocaJugador(){
+    let inputagumon = document.getElementById('agumon')
+    let inputgabumon = document.getElementById('gabumon')
+    let inputfloemon = document.getElementById('floemon')
+    let inputleomon = document.getElementById('leomon')
+    let inputkristalmon = document.getElementById('kristalmon')
+    let inputmememon = document.getElementById('mememon')
+        
+      
+    //Seleccion del compañero    
+        if (inputagumon.checked){
+            alert('Selecionaste a Agumon')
+        }else if (inputgabumon.checked){
+            alert('Selecionast a Gabumon')
+        }else if(inputfloemon.checked){
+            alert('Selecionast a Floemon')
+        }else if(inputleomon.checked){
+            alert('Selecionast a Leomon')
+        }else if(inputkristalmon.checked){
+            alert('Selecionast a Kristalmon')     
+        }else if(inputmememon.checked){
+            alert('Selecionast a Mememon')
+        }else {
+            alert('No elejiste ningun Digimon')
+        }
+}
+        window.addEventListener('load', iniciarJuego)
